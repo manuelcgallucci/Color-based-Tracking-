17832f8be3177a55ff5635e86e11de8067d1242b
msl <manuel.sanchez-laguardia@imt-atlantique.net>
2023-02-22 16:36:30 +0100

    added histogram updating

--- meanshift_video.py
+++ meanshift_video.py
@@ -2,7 +2,10 @@ import numpy as np
 import cv2
 import glob
 
+# name of the video sequence to study
 video_name = 'bag'
+# parameter to determine the rate of change of the reference histogram (lower = less change)
+alpha = 0.1
 
 cap = cv2.VideoCapture('output/'+video_name+'.avi')
 
@@ -21,7 +24,7 @@ track_window = (c,r,w,h)
 
 # # set up the ROI for tracking
 roi = frame[r:r+h, c:c+w]
-hsv_roi =  cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+hsv_roi = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
 roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
 cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
@@ -29,18 +32,28 @@ cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
 # Setup the termination criteria, either 10 iteration or move by at least 1 pt
 term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
 
+# histogram updating parameter
+roi_hist_iter = roi_hist
 while(1):
     ret ,frame = cap.read()
 
     if ret == True:
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
+        dst = cv2.calcBackProject([hsv],[0],roi_hist_iter,[0,180],1)
 
         # apply meanshift to get the new location
         ret, track_window = cv2.meanShift(dst, track_window, term_crit)
 
-        # Draw it on image
+        # update reference histogram
         x,y,w,h = track_window
+        roi = frame[y:y+h, x:x+w]
+        hsv_roi = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+        roi_hist_iter = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
+        cv2.normalize(roi_hist_iter,roi_hist_iter,0,255,cv2.NORM_MINMAX)
+        roi_hist_iter = alpha*roi_hist + (1-alpha)*roi_hist_iter
+
+        # draw it on image
         img2 = cv2.rectangle(frame, (x,y), (x+w,y+h), 255,2)
         cv2.imshow('img2',img2)
 
