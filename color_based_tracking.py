89178ce3df119bb105df032c1258ad91d0b7f624
Manuel Cabeza Gallucci <42854238+manuelcgallucci@users.noreply.github.com>
2023-02-22 16:22:44 +0100

    update color based

--- color_based_tracking.py
+++ color_based_tracking.py
@@ -5,24 +5,26 @@ def calculate_hsv_hist(bgr_img, hist_size):
     hsv_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2HSV)
 
     # Limit H bigger than 0.1 and S bigger than 0.2, No limit for V
-    mask = cv2.inRange(hsv_img, (25, 50, 0), (255, 255, 255))
+    # mask = cv2.inRange(hsv_img, (25, 50, 0), (255, 255, 255))
 
     roi_hist = np.zeros((hist_size,3))
     for i in range(3):
-        roi_hist[:,i] = cv2.calcHist(hsv_img, [i+1], mask, [hist_size], [0,hist_size])[:,0]
+        roi_hist[:,i] = cv2.calcHist(hsv_img, [i+1], None, [hist_size], [0,hist_size])[:,0]
+        roi_hist[:,i] = roi_hist[:,i] / np.sum(roi_hist[:,i])
     # cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
 
     return roi_hist
 
 class ParticleFilter(object):
-    def __init__(self, x0, y0, w0, h0, first_roi_bgr, n_particles=100, dt=1, sigma=[1,1,0.1,0.1], hist_size=64, lambda_=10):
+    def __init__(self, x0, y0, w0, h0, first_roi_bgr, window_size, n_particles=10, dt=1, sigma=[1,1,0.1,0.1], hist_size=64, lambda_=10, \
+                min_size=40, max_size=200):
         # Define the initial state of the system
         self.state = np.array([x0, y0, w0, h0])
         self.n_state = self.state.shape[0]
         # Define the model matrices 
-        self.A = np.array([[1+dt, 0, 0], [0, 1+dt, 0], [0, 0, 1+dt/4]])
-        self.B = np.zeros([[-dt, 0, 0], [0, -dt, 0], [0, 0, -dt/4]])
-        self.C = np.zeros(0)
+        self.A = np.array([[1+dt, 0, 0, 0], [0, 1+dt, 0, 0], [0, 0, 1+dt, 0], [0, 0, 0, 1+dt]])
+        self.B = np.array([[-dt, 0, 0, 0], [0, -dt, 0, 0], [0, 0, -dt, 0], [0, 0, 0, -dt]])
+        # self.C = np.zeros(0)
 
         # Define the noise sigma
         self.sigma_v = np.array(sigma)
@@ -37,12 +39,18 @@ class ParticleFilter(object):
         self.hist_size = hist_size
         self.lambda_ = lambda_
 
+        self.min_size = min_size
+        self.max_size = max_size
+        self.window_size = window_size
+
     def transition_state(self, frame):
         state_pred = self.state_prediction()
-        state_pred = self.correct_size()
+        
+        state_pred = self.correct_size(state_pred)
 
         histograms = self.calculate_hist(state_pred, frame)
         weights = self.compare_hist(histograms, self.hist)
+
         self.last_particles = self.particles
         self.particles = self.resample(state_pred, weights)
 
@@ -63,7 +71,8 @@ class ParticleFilter(object):
         weights = np.zeros((self.n_particles))
         # For the moment only use the first dimension of the histogram
         for m in range(self.n_particles):
-            weights[m] = np.exp(self.lambda_ * np.sqrt(np.sum(histograms[m,:,0]* histogram_ref[:,])))
+            weights[m] = np.exp(self.lambda_ * np.sqrt(np.sum(histograms[m,:,0]* histogram_ref[:,0])))
+        
         return weights / np.sum(weights)
 
     def calculate_hist(self, state_pred, frame):
@@ -71,19 +80,28 @@ class ParticleFilter(object):
         for k, state in enumerate(state_pred):
             # Crop roi into the frame
             # state_pred is: (x,y, w, h)
-            bgt_roi_cropped = frame[int(state_pred[0]):int(state_pred[0]+state_pred[2]), int(state_pred[1]):int(state_pred[1]+state_pred[3])]
+            init_x = min([self.window_size[0], max([0, int(state[0])])])
+            max_x = min([self.window_size[0], max([1, int(state[0] + state[2])])])
+            init_y = min([self.window_size[1], max([0, int(state[1])])])
+            max_y = min([self.window_size[1], max([1, int(state[1] + state[3])])])
+            
+            
+            # print(init_x, max_x, init_y, max_y)
+            bgt_roi_cropped = frame[init_x:max_x, init_y:max_y]
+            # print('\t', bgt_roi_cropped.shape, state)
             # Calculate and append the histogram
             histograms[k,:,:] = calculate_hsv_hist(bgt_roi_cropped, self.hist_size)
         return histograms
 
     def state_prediction(self):
         noise = self.sigma_v*np.random.randn(self.n_particles,self.n_state)
-        particles = np.dot(self.particles, self.A) + np.dot(self.last_particles) + noise
+        particles = np.dot(self.particles, self.A) + np.dot(self.last_particles, self.B) + noise
         return particles
     
     def correct_size(self, predictions):
         """ Correct the size so the bb remains in the image"""
         # np.clip(predictions[:,0],self.state[2]+1,self.window_size[0]-(1+self.state[2]),predictions[:,0])        
         # np.clip(predictions[:,1],self.state[2]+1,self.window_size[1]-(1+self.state[2]),predictions[:,1])
-        # np.clip(predictions[:,2],self.min_square,self.max_square,predictions[:,2])
+        np.clip(predictions[:,2], self.min_size, self.max_size,predictions[:,2])
+        np.clip(predictions[:,3], self.min_size, self.max_size,predictions[:,3])
         return predictions
--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def main(hist_size=64):
     # ==== Initialization ===
     cap = cv2.VideoCapture(0)
     ret,frame = cap.read()
-    
+    window_size = (int(cap.get(3)), int(cap.get(4)))
+    print(window_size)
     if not ret:
         print("Camera not found")
         return
@@ -28,16 +29,31 @@ def main(hist_size=64):
 
     roi_cropped = frame[int(roi_coord[1]):int(roi_coord[1]+roi_coord[3]), int(roi_coord[0]):int(roi_coord[0]+roi_coord[2])]
 
-    roi_hist = calculate_hsv_hist(roi_cropped, hist_size)
-    plot_color_hist(roi_hist)
-
+    # roi_hist = calculate_hsv_hist(roi_cropped, hist_size)
+    # plot_color_hist(roi_hist)
+    print("Coords:", roi_coord)
+    
     # Define the intial conditions of the model
-    pFilter = ParticleFilter(roi_coord[1], roi_coord[0], roi_coord[3], roi_coord[2], roi_cropped, n_particles=100, dt=1, sigma=[1,1,0.1,0.1], hist_size=64, lambda_=10)
+    pFilter = ParticleFilter(roi_coord[1], roi_coord[0], roi_coord[3], roi_coord[2], roi_cropped, \
+            window_size=window_size, n_particles=100, dt=0.01, sigma=[1,1,0.1,0.1], hist_size=64, lambda_=20)
+    
     # ==== Loop ===
     while(True):        
         ret, frame = cap.read()
         
-        x,y,w,h,predictions_resampled, predictions = pf.next_state(frame)
+        x,y,w,h,predictions_resampled, predictions = pFilter.transition_state(frame)
+
+        cv2.rectangle(frame,x+w,y+h,(0,0,255),thickness=2)
+
+        for n in range(pFilter.n_particles):
+            cx = int(predictions[n,0] + predictions[n,2] / 2.0)
+            cy = int(predictions[n,1] + predictions[n,3] / 2.0)
+            cv2.circle(frame, (cx,cy), radius=4, color =(0,255,0), thickness=-1)
+
+        for n in range(pFilter.n_particles):
+            cx = int(predictions_resampled[n,0] + predictions_resampled[n,2] / 2.0)
+            cy = int(predictions_resampled[n,1] + predictions_resampled[n,3] / 2.0)
+            cv2.circle(frame, (cx,cy), radius=1, color =(255,0,0), thickness=-1)
 
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
