9fbd6ba556d5ef563efd1ee1977f8eb6a4541812
Manuel SANCHEZ LAGUARDIA <m21sanc3@vl-tp-br-005.imta.fr>
2023-03-08 16:05:17 +0100

    Update img2video.py and meanshift_video.py

--- img2video.py
+++ img2video.py
@@ -3,7 +3,7 @@ import numpy as np
 import glob
 
 # Set specific video parameters:
-video_sequence_name = 'bag'
+video_sequence_name = name = str(input("Enter filename of video sequence to process: "))
 dir_ = './sequences-train/'
 
 img_array = []
@@ -15,8 +15,8 @@ for filename in glob.glob(dir_+ video_sequence_name +'/*.bmp'):
 
 assert 'frameSize' in globals(), "Images not found or not loaded correctly"
 
-out = cv2.VideoWriter('./output/bag.mp4', cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 30, frameSize)
- 
+out = cv2.VideoWriter('./output/'+video_sequence_name+'.mp4', cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 24, frameSize)
+
 for i in range(len(img_array)):
     out.write(img_array[i])
 out.release()
