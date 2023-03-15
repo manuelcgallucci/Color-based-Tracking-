87fa40ce8937b82f6fb9d51579e915338554ad80
Manuel Cabeza Gallucci <42854238+manuelcgallucci@users.noreply.github.com>
2023-03-15 11:30:43 +0100

    Update main_video.py

--- main_video.py
+++ main_video.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 from color_based_tracking import ParticleFilter, calculate_hsv_hist
 from functions import get_bb_from_mask, get_bb_score
 
-# Todo use HSV channels (take the mean)
-# Correct the boundries of the color based so we dont go out 
 
 def plot_color_hist(histograms):
     # define colors to plot the histograms
@@ -25,22 +23,23 @@ def main(dir_imgs, dir_masks, video_name, type_='probabilistic', hist_size=64):
 	type_ = 'probabilistic' or 'meanshift'
 	'''
 	# ==== Initialization ===
-	print(glob.glob(dir_imgs + video_name +'/*.bmp'))
 	filename = glob.glob(dir_imgs + video_name +'/*.bmp')[0]
-	img = cv2.imread(filename)
-	height, width, layers = img.shape
+	frame = cv2.imread(filename)
+	height, width, layers = frame.shape
 	frameSize = (width,height)
 
 	# select initial location of ROI
+	mask_path = dir_masks + video_name +"/"+ video_name + '-'+ str(1).zfill(3) + '.png'
 	roi_coord = get_bb_from_mask(mask_path)
+	# roi_coord: is x,y,w,h
 	r,h,c,w = int(roi_coord[1]),int(roi_coord[3]),int(roi_coord[0]),int(roi_coord[2])
-	track_window = (c,r,w,h)
+	# track_window = (c,r,w,h)
 	roi = frame[r:r+h, c:c+w]
 
 	# Initialize performance metrics and outputs
 	score = []
 	fps = 24
-	writer = cv2.VideoWriter('./output/'+video_name+'_meanshift.mp4', cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), fps, frameSize)
+	video_writer = cv2.VideoWriter('./output/{:s}_{:s}.mp4'.format(video_name, type_), cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), fps, frameSize)
 
 	# roi_hist = calculate_hsv_hist(roi, hist_size)
 	# plot_color_hist(roi_hist)
@@ -48,38 +47,55 @@ def main(dir_imgs, dir_masks, video_name, type_='probabilistic', hist_size=64):
 
 	# Define the intial conditions of the model
 	if type_== 'probabilistic':
-		pFilter = ParticleFilter(roi_coord[1], roi_coord[0], roi_coord[3], roi_coord[2], roi, \
-			window_size=frameSize, n_particles=100, dt=0.01, sigma=[10,10,1,1], hist_size=hist_size, lambda_=20)
 
+		pFilter = ParticleFilter(roi_coord[0], roi_coord[1], roi_coord[2], roi_coord[3], roi, \
+            window_size=frameSize, n_particles=500, dt=0.2, sigma=[50,50,5.5,5.5], hist_size=16, lambda_=20, 
+            min_size_x=int(roi_coord[2]*0.8), max_size_x=int(roi_coord[2]*1.2),
+            min_size_y=int(roi_coord[3]*0.8), max_size_y=int(roi_coord[3]*1.2))
+		
 	# ==== Loop ===
 	for i, filename in enumerate(glob.glob(dir_imgs + video_name +'/*.bmp')):
+		if i == 0:
+			continue
 		# Read frame
 		frame = cv2.imread(filename)
 		
 		# Predict bounding box
 		if type_== 'probabilistic':
-			x,y,w,h,predictions_resampled, predictions = pFilter.transition_state(frame)
+			x,y,w,h,predictions_resampled, predictions, weights = pFilter.transition_state(frame)
 		elif type_== 'meanshift':
 			pass
 		
 		# Compare to mask and append score
-		mask_path = dir_masks + video_name + '-'+ str(i).zfill(3) + '.png'
+		mask_path = dir_masks + video_name +"/"+ video_name + '-'+ str(i+1).zfill(3) + '.png'
+		print(mask_path)
 		xm, ym, wm, hm = get_bb_from_mask(mask_path)
 		score.append(get_bb_score(x,y,w,h, xm, ym, wm, hm))
 		# TODO: add output centroids.npy
+		
+		
 		
 		# Draw rectangle
 		cv2.rectangle(frame,(int(x),int(y)),(int(x+w),int(y+h)),(0,0,255),thickness=2)
 		
 		# write frame to output
-		writer.write(frame)
+		video_writer.write(frame)
 		
 		cv2.imshow('frame',frame)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break      
 	
-	writer.release()
+	video_writer.release()
 
+	plt.figure()
+	plt.plot(score)
+	plt.title("Score")
+	plt.show()
+	plt.close()
 
 if __name__=="__main__":
-	main()
+	dir_imgs = "./sequences-train/"
+	dir_mask = "./sequences-train/"
+	video_name = "rhino"
+	type_ = "probabilistic"
+	main(dir_imgs, dir_mask, video_name, type_=type_)
