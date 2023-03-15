8d3043fed379c12942c1b35bc67983c3c12bc58f
q21santa <quentin.santarelli@imt-atlantique.net>
2023-03-15 14:48:40 +0100

    correcting meanshift

--- functions.py
+++ functions.py
@@ -44,6 +44,8 @@ import matplotlib.pyplot as plt
 plt.plot(score, title= "distance between bb center through iterations")
 plt.show()
 '''
+
+
 def meanshift_function(video_name, alpha):
     cap = cv2.VideoCapture('output/'+video_name+'.mp4')
     mask_path = 'sequences-train/'+ video_name +'/'+ video_name + '-'+ str(1).zfill(3) + '.png'
@@ -54,63 +56,53 @@ def meanshift_function(video_name, alpha):
 
     # select initial location of window
     roi_coord = get_bb_from_mask(mask_path)
-    #cv2.destroyAllWindows()
-
-    # setup initial location of window
-    # r,h,c,w - region of image
-    #           simply hardcoded the values
     r,h,c,w = int(roi_coord[1]),int(roi_coord[3]),int(roi_coord[0]),int(roi_coord[2])
     track_window = (c,r,w,h)
-
-    # # set up the ROI for tracking
-    roi = frame[r:r+h, c:c+w]
-    hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
-    roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
-    cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
-
     # Setup the termination criteria, either 10 iteration or move by at least 1 pt
     term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
 
-    # Writer to save video file:
-    # get cap properties
-    width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))   # float `width` to int
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # float `height` to int
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    frameSize = (width,height)
+
+    ## INITIALISATION
+    roi = frame[r:r+h, c:c+w]
+    hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+    init_roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
+    cv2.normalize(init_roi_hist,init_roi_hist,0,255,cv2.NORM_MINMAX)
 
     score = []
-    i=1
-    # histogram updating parameter
-    roi_hist_iter = roi_hist
+    roi_hist_iter = init_roi_hist
+    i = 1
+
     while(1):
         ret ,frame = cap.read()
 
         if ret == True:
+            # MEANSHIFT
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             dst = cv2.calcBackProject([hsv],[0],roi_hist_iter,[0,180],1)
-
-            # apply meanshift to get the new location
             ret, track_window = cv2.meanShift(dst, track_window, term_crit)
+            
 
-            # update reference histogram
+
+            ## UPDATING THE HISTOGRAM
             x,y,w,h = track_window
-            mask_path = 'sequences-train/'+ video_name +'/'+ video_name + '-'+ str(i).zfill(3) + '.png'
-
-            xm, ym, wm, hm = get_bb_from_mask(mask_path)
-            score.append(get_bb_score(x,y,w,h, xm, ym, wm, hm))
-
             roi = frame[y:y+h, x:x+w]
-            hsv_roi = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
             mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
             roi_hist_iter = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
             cv2.normalize(roi_hist_iter,roi_hist_iter,0,255,cv2.NORM_MINMAX)
-            # Updated histogram is proportional to the original and the recalculated in this frame by a factor of alpha
-            roi_hist_iter = alpha*roi_hist + (1-alpha)*roi_hist_iter
 
-            # draw it on image
+            # Updated histogram is proportional to the original and the recalculated in this frame by a factor of alpha
+            roi_hist_iter = alpha*init_roi_hist + (1-alpha)*roi_hist_iter
+
+            ## DRAWING
             img2 = cv2.rectangle(frame, (x,y), (x+w,y+h), 255,2)
-            cv2.imshow('img2',img2)
+            cv2.imshow('Image video',img2)
+            
+            ## SCORE
+            mask_path = 'sequences-train/'+ video_name +'/'+ video_name + '-'+ str(i).zfill(3) + '.png'
+            xm, ym, wm, hm = get_bb_from_mask(mask_path)
+            score.append(get_bb_score(x,y,w,h, xm, ym, wm, hm))
 
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -124,6 +116,5 @@ def meanshift_function(video_name, alpha):
     cv2.destroyAllWindows()
     cap.release()
     print('One meanshift have been performed')
-
 
     return score
