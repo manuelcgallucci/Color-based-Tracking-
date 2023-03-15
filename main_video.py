892d7046adbd6818f6dce16a3e09be67f45077dd
Manuel S <manu3z@github.com>
2023-03-15 16:51:33 +0100

    working algorithm for meanshift

--- main_video.py
+++ main_video.py
@@ -17,7 +17,7 @@ def plot_color_hist(histograms):
     plt.title('Image Histogram ' + names[0]+names[1]+names[2])
     plt.show()
 
-def main(dir_imgs, dir_masks, video_name, type_='probabilistic', hist_size=64, show=False):
+def main(dir_imgs, dir_masks, video_name, type_='probabilistic', hist_size=180, show=False):
 	'''
 	dir_imgs and dir_masks with '/' at the end
 	type_ = 'probabilistic' or 'meanshift'
@@ -41,10 +41,6 @@ def main(dir_imgs, dir_masks, video_name, type_='probabilistic', hist_size=64, s
 	fps = 24
 	video_writer = cv2.VideoWriter('./output/{:s}/{:s}_{:s}.mp4'.format(type_, video_name, type_), cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), fps, frameSize)
 
-	# roi_hist = calculate_hsv_hist(roi, hist_size)
-	# plot_color_hist(roi_hist)
-	# print("Coords:", roi_coord)
-
 	# Define the intial conditions of the model
 	if type_== 'probabilistic':
 
@@ -56,17 +52,20 @@ def main(dir_imgs, dir_masks, video_name, type_='probabilistic', hist_size=64, s
 	elif type_=='meanshift':
 		# Setup the ROI for tracking
 		hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-		mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
-		roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
-		cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
+		mask = cv2.inRange(hsv_roi, np.array((0., 25., 50.)), np.array((180.,255.,255.)))
+		init_roi_hist = cv2.calcHist([hsv_roi],[0],mask,[hist_size],[0,hist_size])
+		cv2.normalize(init_roi_hist,init_roi_hist,0,255,cv2.NORM_MINMAX)
+
 		# Setup the termination criteria, either 10 iteration or move by at least 1 pt
 		term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+
+		roi_hist_iter = init_roi_hist
 
 	# ==== Loop ===
 	for i, filename in enumerate(glob.glob(dir_imgs + video_name +'/*.bmp')):
 		# Skip first image
-		if i == 0:
-			continue
+		# if i == 0:
+		# 	continue
 		
 		# Read frame
 		frame = cv2.imread(filename)
@@ -75,25 +74,28 @@ def main(dir_imgs, dir_masks, video_name, type_='probabilistic', hist_size=64, s
 		if type_== 'probabilistic':
 			x,y,w,h,predictions_resampled, predictions, weights = pFilter.transition_state(frame)
 		elif type_== 'meanshift':
-			track_window = meanshift(frame, track_window, roi_hist, term_crit)
+			track_window, roi_hist_iter = meanshift(frame, track_window, init_roi_hist, roi_hist_iter, term_crit, alpha=1)
 			x,y,w,h = track_window
+			# print(track_window)
 		
 		# Compare to mask and append score
 		mask_path = dir_masks + video_name +"/"+ video_name + '-'+ str(i+1).zfill(3) + '.png'
 		xm, ym, wm, hm = get_bb_from_mask(mask_path)
 		score.append(get_bb_score(x,y,w,h, xm, ym, wm, hm))
 		# TODO: add output centroids.npy
-		
+
 		# Draw rectangle
 		cv2.rectangle(frame,(int(xm),int(ym)),(int(xm+wm),int(ym+hm)),(0,255,0),thickness=2)
 		cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), thickness=2)
+
 		# write frame to output
 		video_writer.write(frame)
 		
 		if show: cv2.imshow('frame',frame)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break      
-	
+			break
+		
+	#  plot_color_hist(roi_hist_iter)
 	video_writer.release()
 
 	plt.figure()
@@ -107,9 +109,10 @@ def main(dir_imgs, dir_masks, video_name, type_='probabilistic', hist_size=64, s
 if __name__=="__main__":
 	dir_imgs = "./sequences-train/"
 	dir_mask = "./sequences-train/"
-	video_names = ["bag"]#, "book", "bear", "bag", "camel", "rhino", "swan"]
+	video_names = ["bag", "book", "bear", "bag", "camel", "rhino", "swan"]
 	type_ = "meanshift"
 	show = False
+	hist_size = 180
 	
 	for video_name in video_names:		
 		main(dir_imgs, dir_mask, video_name, type_=type_, show=show)
--- meanshift.py
+++ meanshift.py
@@ -1,11 +1,22 @@
 import cv2
+import numpy as np
 
-def meanshift(frame, track_window, roi_hist, term_crit):
-
+def meanshift(frame, track_window, init_roi_hist, roi_hist_iter, term_crit, alpha=1):
+    # MEANSHIFT
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
-
-    # apply meanshift to get the new location
+    #print(roi_hist_iter[:,0])
+    dst = cv2.calcBackProject([hsv],[0,1],roi_hist_iter[:,:1],[0,180],1)
     ret, track_window = cv2.meanShift(dst, track_window, term_crit)
 
-    return track_window
+    ## UPDATING THE HISTOGRAM
+    x,y,w,h = track_window
+    roi = frame[y:y+h, x:x+w]
+    hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv_roi, np.array((0., 25.,50.)), np.array((180.,255.,255.)))
+    roi_hist_iter = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
+    cv2.normalize(roi_hist_iter,roi_hist_iter,0,255,cv2.NORM_MINMAX)
+
+    # Updated histogram is proportional to the original and the recalculated in this frame by a factor of alpha
+    roi_hist_iter = alpha*init_roi_hist #+ (1-alpha)*roi_hist_iter
+
+    return track_window, roi_hist_iter
